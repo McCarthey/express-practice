dcd5e5c183eb715a80a01d430bc4c876ea19aa28
mccarthey <mccarthey123@outlook.com>
2018-04-21 15:49:26 +0800

    hello world

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ const app = express();
 app.get('/',(req,res)=>{
     res.send('Hello World')
 })
+
+const server = app.listen(3000,()=>{
+    const host = server.address().address;
+    const port = server.address().port;
+
+    console.log('start on http://%s:%s',host,port);
+
+});
