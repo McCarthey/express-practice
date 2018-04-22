deb9d215691a619b5182912e924840949e4a4139
mccarthey <mccarthey123@outlook.com>
2018-04-22 11:55:24 +0800

    静态文件托管

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send('Hello World')
-})
+// app.get('/',(req,res)=>{
+//     res.send('Hello World')
+// })
+
+app.use(express.static('public'))
 
 const server = app.listen(3000,()=>{
     const host = server.address().address;
