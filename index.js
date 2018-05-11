9b8c7b4969cb16c8277367f8015d976efb39bedd
mccarthey <mccarthey123@outlook.com>
2018-05-11 22:46:16 +0800

    get请求返回json格式对象

--- index.js
+++ index.js
@@ -1,45 +1,49 @@
-const express = require("express")
+const express = require('express')
 const app = express()
-const bodyParser = require("body-parser")
+const bodyParser = require('body-parser')
 
 const jsonParser = bodyParser.json()
 const urlEncodedParser = bodyParser.urlencoded({ extended: false })
 
-app.post("/", (req, res) => {
-    res.send("Post request to the homepage")
+app.get('/user/list', (req, res) => {
+  res.contentType('json')
+  res.send({ title: 'user list' })
+})
+app.post('/', (req, res) => {
+  res.send('Post request to the homepage')
 })
 
-app.post("/login", urlEncodedParser, (req, res) => {
-    if (!req.body) {
-        return res.sendStatus(400)
-    }
-    if (!req.body.username) {
-        res.send(`Please upload your name`)
-    } else {
-        res.send(`welcome, ${req.body.username}`)
-    }
+app.post('/login', urlEncodedParser, (req, res) => {
+  if (!req.body) {
+    return res.sendStatus(400)
+  }
+  if (!req.body.username) {
+    res.send(`Please upload your name`)
+  } else {
+    res.send(`welcome, ${req.body.username}`)
+  }
 })
 
-app.post("/users", jsonParser, (req, res) => {
-    if (!req.body) {
-        return res.sendStatus(400)
-    }
-    console.log(req.body)
-    res.send(req.body)
+app.post('/users', jsonParser, (req, res) => {
+  if (!req.body) {
+    return res.sendStatus(400)
+  }
+  console.log(req.body)
+  res.send(req.body)
 })
 
-app.use(express.static("public"))
+app.use(express.static('public'))
 
 // 404
 app.use((req, res, next) => {
-    res.status(404).send("Oops,page not found")
+  res.status(404).send('Oops,page not found')
 })
 
 const server = app.listen(3000, () => {
-    const host = server.address().address
-    const port = server.address().port
+  const host = server.address().address
+  const port = server.address().port
 
-    console.log(`start on http://${host}:${port}`)
+  console.log(`start on http://${host}:${port}`)
 })
 
 /**
