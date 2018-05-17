5aa61f05b1b9a0aa1cfa552fd80c696ad5ffa51c
Dean <mccarthey123@outlook.com>
2018-05-17 20:33:05 +0800

    format

--- index.js
+++ index.js
@@ -1,60 +1,60 @@
-const express = require('express')
+const express = require("express")
 const app = express()
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser")
 
 const jsonParser = bodyParser.json()
 const urlEncodedParser = bodyParser.urlencoded({ extended: false })
 
 // post方法
-app.post('/', (req, res) => {
-  res.send('Post request to the homepage')
+app.post("/", (req, res) => {
+    res.send("Post request to the homepage")
 })
 // get方法
-app.get('/user/list', (req, res) => {
-  res.contentType('json')
-  res.send({ title: 'user list' })
+app.get("/user/list", (req, res) => {
+    res.contentType("json")
+    res.send({ title: "user list" })
 })
 // 带参数路由
-app.get('/user/:id', (req, res) => {
-  console.log(req.params.id)
-  let id = req.params.id
-  res.send(`Welcome user ${id}`)
+app.get("/user/:id", (req, res) => {
+    console.log(req.params.id)
+    let id = req.params.id
+    res.send(`Welcome user ${id}`)
 })
 
 // post urlEncodedParser
-app.post('/login', urlEncodedParser, (req, res) => {
-  if (!req.body) {
-    return res.sendStatus(400)
-  }
-  if (!req.body.username) {
-    res.send(`Please upload your name`)
-  } else {
-    res.send(`welcome, ${req.body.username}`)
-  }
+app.post("/login", urlEncodedParser, (req, res) => {
+    if (!req.body) {
+        return res.sendStatus(400)
+    }
+    if (!req.body.username) {
+        res.send(`Please upload your name`)
+    } else {
+        res.send(`welcome, ${req.body.username}`)
+    }
 })
 
 // post json
-app.post('/users', jsonParser, (req, res) => {
-  if (!req.body) {
-    return res.sendStatus(400)
-  }
-  console.log(req.body)
-  res.send(req.body)
+app.post("/users", jsonParser, (req, res) => {
+    if (!req.body) {
+        return res.sendStatus(400)
+    }
+    console.log(req.body)
+    res.send(req.body)
 })
 
 // 静态文件中间件
-app.use(express.static('public'))
+app.use(express.static("public"))
 
 // 404
 app.use((req, res, next) => {
-  res.status(404).send('Oops,page not found')
+    res.status(404).send("Oops,page not found")
 })
 
 const server = app.listen(3000, () => {
-  const host = server.address().address
-  const port = server.address().port
+    const host = server.address().address
+    const port = server.address().port
 
-  console.log(`start on http://${host}:${port}`)
+    console.log(`start on http://${host}:${port}`)
 })
 
 /**
